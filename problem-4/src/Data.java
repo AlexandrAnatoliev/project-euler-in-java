1ff9e8dcf365e257b9d51febba6321d95a253c7b
AlexandrAnatoliev <per-1986@list.ru>
2025-01-06 20:53:10 +0300

    feat: add "Data" class
    class is used to storage search range
    
    version 0.2.0

---
+++ problem-4/src/Data.java
@@ -0,0 +1,23 @@
+public class Data {
+    private int maxNum;
+    private int minNum;
+
+    public Data() {
+    }
+
+    public void setMaxNum(int maxNum) {
+        this.maxNum = maxNum;
+    }
+
+    public void setMinNum(int minNum) {
+        this.minNum = minNum;
+    }
+
+    public int getMaxNum() {
+        return maxNum;
+    }
+
+    public int getMinNum() {
+        return minNum;
+    }
+}
