1b3f715edf12e4999c22b98c7ee33b881c085c60
AlexandrAnatoliev <per-1986@list.ru>
2025-01-06 19:38:28 +0300

    feat: program runs and returns correct answer
    version 0.1.0

--- problem-4/src/Problem4.java
+++ problem-4/src/Problem4.java
@@ -1,2 +1,35 @@
+/**
+ * project Euler problem 3
+ *
+ * @author AlexandrAnatoliev
+ * @version 0.1.0 2025-01-06
+ */
+
 public class Problem4 {
+    public static void main(String[] args) {
+
+        int answer = 0;
+        int num = 999;
+
+        while (answer < num * num && num > 99) {
+            for (int i = num; i > 99; i--) {
+                if (isPalindrome(String.valueOf(num * i))) {
+                    answer = Math.max(i * num, answer);
+                    break;
+                }
+            }
+            num--;
+        }
+        System.out.println(answer);
+    }
+
+    public static String reverse(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    public static boolean isPalindrome(String string) {
+        return string.equals(reverse(string));
+    }
+
+
 }
