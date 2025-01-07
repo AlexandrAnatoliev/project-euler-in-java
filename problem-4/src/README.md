6b5ab271441db4e8e06af251df47c2c0a56fd934
AlexandrAnatoliev <per-1986@list.ru>
2025-01-07 21:53:04 +0300

    docs: update README file and add comments
    version 0.4.2

--- problem-4/src/README.md
+++ problem-4/src/README.md
@@ -13,5 +13,21 @@ The largest palindrome made from the product of **two 2-digit** numbers is **900
 Find the largest palindrome made from the product of **two 3-digit** numbers.
 
 <div id="header" align="center">
-    <h3>Алгоритм решения</h3>
+    <h3>Solving algorithm</h3>
 </div>
+
+The program algorithm is based on enumerating the factors and searching for the answer amount them.
+
+Since the factors go from larger to smaller, when searching number the first answer found will stop the cycle **for()**. 
+Because each subsequent factor will be less than previous one and the product **fact1 * fact2**, respectively, too. 
+The search for **fact1** will stop when the product of the factors **fact1 * fact1** exceeds the already founded maximum **answer** for same reason.
+
+<div id="header" align="center">
+    <h3>New skills</h3>
+</div>
+
+* wrote docs in English 
+* enum and package was used
+* the user interface was separated into a class
+
+
--- problem-4/src/problem4/Main.java
+++ problem-4/src/problem4/Main.java
@@ -3,9 +3,8 @@ package problem4;
  * project Euler problem 4
  *
  * @author AlexandrAnatoliev
- * @version 0.4.1 2025-01-06
+ * @version 0.4.2 2025-01-06
  */
-
 public class Main {
     public static void main(String[] args) {
 
@@ -28,14 +27,25 @@ public class Main {
                 " and " + data.getMaxNum() + " is " + answer);
     }
 
+    /**
+     * Method inverts natural number
+     *
+     * @param number Natural number
+     * @return Inverted number
+     */
     public static int invert(int number) {
         String stringNumber = String.valueOf(number);
         stringNumber = new StringBuilder(stringNumber).reverse().toString();
         return Integer.parseInt(stringNumber);
     }
 
+    /**
+     * Method checks if number is palindrome
+     *
+     * @param number Number being checking
+     * @return True, if number is palindrome
+     */
     public static boolean isPalindrome(int number) {
         return number == invert(number);
     }
-
 }
