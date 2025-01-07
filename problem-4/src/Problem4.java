6774487d1490c584435df08ff08d02c8d8ee12f2
AlexandrAnatoliev <per-1986@list.ru>
2025-01-07 20:28:15 +0300

    BREAING_CHANGE: replace "reverse" function to "invert"
    change input and return values of functions from String to int
    
    version 0.4.0

--- problem-4/src/Problem4.java
+++ problem-4/src/Problem4.java
@@ -1,23 +1,23 @@
 /**
- * project Euler problem 3
+ * project Euler problem 4
  *
  * @author AlexandrAnatoliev
- * @version 0.3.2 2025-01-06
+ * @version 0.4.0 2025-01-06
  */
 
 public class Problem4 {
     public static void main(String[] args) {
 
         UserInterface userInterface = new UserInterface();
-
         Data data = userInterface.setUserInput();
+
         int answer = 0;
         int fact1 = (int) Math.sqrt(data.getMaxNum());
 
         while (answer < fact1 * fact1) {
             for (int fact2 = fact1; fact1 * fact2 > data.getMinNum(); fact2--) {
-                if (isPalindrome(String.valueOf(fact1 * fact2))) {
-                    answer = Math.max(fact2 * fact1, answer);
+                if (isPalindrome(fact1 * fact2)) {
+                    answer = Math.max(fact1 * fact2, answer);
                     break;
                 }
             }
@@ -27,11 +27,14 @@ public class Problem4 {
                 " and " + data.getMaxNum() + " is " + answer);
     }
 
-    public static String reverse(String string) {
-        return new StringBuilder(string).reverse().toString();
+    public static int invert(int number) {
+        String stringNumber = String.valueOf(number);
+        stringNumber = new StringBuilder(stringNumber).reverse().toString();
+        return Integer.parseInt(stringNumber);
     }
 
-    public static boolean isPalindrome(String string) {
-        return string.equals(reverse(string));
+    public static boolean isPalindrome(int number) {
+        return number == invert(number);
     }
+
 }
