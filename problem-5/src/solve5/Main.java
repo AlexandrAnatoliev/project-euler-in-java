0cf7027e07a6fd3ec806ff9990804c1ffade431b
AlexandrAnatoliev <per-1986@list.ru>
2025-01-08 19:53:34 +0300

    feat: program return correct answer

--- problem-5/src/solve5/Main.java
+++ problem-5/src/solve5/Main.java
@@ -2,6 +2,25 @@ package solve5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        long answ = 1;
+        for (int i = 2; i < 20; i++) {
+            answ = getLCM(answ, i);
+        }
+        System.out.println(answ);
+    }
+
+    public static long getGCD(long num1, long num2) {
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) {
+                num1 %= num2;
+            } else {
+                num2 %= num1;
+            }
+        }
+        return Math.max(num1, num2);
+    }
+
+    public static long getLCM(long num1, long num2) {
+        return (num1 * num2) / getGCD(num1, num2);
     }
 }
