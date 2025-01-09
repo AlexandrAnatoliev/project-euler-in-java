4db9fc4f36bbed3cd4fc65c43c99f949f3e795fb
AlexandrAnatoliev <per-1986@list.ru>
2025-01-09 20:48:28 +0300

    feat: add UserInterface class
    version 0.3.2

--- problem-5/src/solve5/Main.java
+++ problem-5/src/solve5/Main.java
@@ -4,20 +4,19 @@ package solve5;
  * project Euler problem 5
  *
  * @author AlexandrAnatoliev
- * @version 0.3.0 2025-01-08
+ * @version 0.3.2 2025-01-08
  */
 public class Main {
     public static void main(String[] args) {
         long answ = 1;
 
-        Data data = new Data();
-        data.setFirstNum(2);
-        data.setPastNum(20);
+        UserInterface userInterface = new UserInterface();
+        Data data = userInterface.setUserInput();
+
         LeastCommonMultiple lcm = new LeastCommonMultiple();
         for (long i = data.getFirstNum(); i < data.getPastNum(); i++) {
             answ = lcm.get(answ, i);
         }
         System.out.println(answ);
     }
-
 }
