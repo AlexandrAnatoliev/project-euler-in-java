a74c156d4d0044290ccb9fb35590574913fe3f59
AlexandrAnatoliev <per-1986@list.ru>
2025-01-09 22:17:19 +0300

    fix: add catch user input of negative numbers
    version 0.4.1

--- problem-5/src/solve5/Main.java
+++ problem-5/src/solve5/Main.java
@@ -6,11 +6,10 @@ import java.util.InputMismatchException;
  * project Euler problem 5
  *
  * @author AlexandrAnatoliev
- * @version 0.4.0 2025-01-08
+ * @version 0.4.1 2025-01-08
  */
 public class Main {
     public static void main(String[] args) {
-//        long answer = 1;
 
         Data data;
         UserInterface userInterface = new UserInterface();
@@ -23,7 +22,14 @@ public class Main {
         }
 
         Answer answer = new Answer();
-        long answ = answer.get(data.getFirstNum(), data.getPastNum());
+        long answ;
+        try {
+            answ = answer.get(data.getFirstNum(), data.getPastNum());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Use positive number only!");
+            data = userInterface.setUserInput();
+            answ = answer.get(data.getFirstNum(), data.getPastNum());
+        }
 
         System.out.println(answ + " is the smallest number that can be divided by each of the numbers from " +
                 data.getFirstNum() + " to " + data.getPastNum() + " without any remainder.");
