3d945a1c78f160f5e9a4955a70ad46202df40ca9
AlexandrAnatoliev <per-1986@list.ru>
2025-04-29 21:10:54 +0300

    docs: update readme file

--- problem-3/src/main/java/Solution.java
+++ problem-3/src/main/java/Solution.java
@@ -2,7 +2,7 @@
  * project Euler problem 3
  *
  * @author AlexandrAnatoliev
- * @version 0.1.1 2025-04-28
+ * @version 0.1.2 2025-04-28
  */
 public class Solution {
     public static void main(String[] args) {
