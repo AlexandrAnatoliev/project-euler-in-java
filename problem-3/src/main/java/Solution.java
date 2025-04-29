49ed3d7cac2b2f51a9f7448f9a875de03eb16f44
AlexandrAnatoliev <per-1986@list.ru>
2025-04-29 22:22:39 +0300

    feat: add user input by command line

--- problem-3/src/main/java/Solution.java
+++ problem-3/src/main/java/Solution.java
@@ -2,12 +2,13 @@
  * project Euler problem 3
  *
  * @author AlexandrAnatoliev
- * @version 0.2.0 2025-04-29
+ * @version 0.3.0 2025-04-29
  */
 public class Solution {
     public static void main(String[] args) {
+    	
+    	long num = Long.parseLong(args[0]);
 
-        long num = 600_851_475_143L;
         Calculator calculator = new Calculator();
         long answ = calculator.getMaxPrimeDiv(num);
 
