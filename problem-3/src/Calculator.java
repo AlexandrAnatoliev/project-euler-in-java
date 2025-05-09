6a907a6eefa9aa08339855c19f9813868960c55c
AlexandrAnatoliev <per-1986@list.ru>
2025-05-09 20:12:25 +0300

    fix: replase if-then construction

--- problem-3/src/Calculator.java
+++ problem-3/src/Calculator.java
@@ -10,13 +10,9 @@ class Calculator {
         if(number % div == 0) {
         	return div;
         }
-        else if (div % 2 == 0){
-        	div++;
-        }
-        else {
-        	div += 2;
-        }
 
+        div = (div % 2 == 0) ? (div + 1) : (div + 2);
+        
         for (long i = div; i * i < number; i += 2) {
             if (number % i == 0)
                 return i;
