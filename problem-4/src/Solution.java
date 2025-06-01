e0cc313fe984f66cbce60152eccde7e718f7e61d
galaxy tab <per-1986@list.ru>
2025-06-01 15:33:33 +0000

    feat: solves problem 4

---
+++ problem-4/src/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * project Euler problem 4
+ *
+ * @author AlexandrAnatoliev
+ * @version 0.5.0 2025-01-06
+ */
+public class Solution {
+	public static void main(String[] args) {
+		int limit = 999999;
+    int answer = 0;
+    int fact1 = (int) Math.sqrt(limit);
+
+    while (answer < fact1 * fact1) {
+      for (int fact2 = fact1; fact1 * fact2 > 99999; fact2--) {
+        if (isPalindrome(fact1 * fact2)) {
+          answer = Math.max(fact1 * fact2, answer);
+            break;
+        }
+      }
+      fact1--;
+    }
+    System.out.println("The largest palindrome between " + 99999 +
+                " and " + 999999 + " is " + answer);
+  }
+
+    /**
+     * Method inverts natural number
+     *
+     * @param number Natural number
+     * @return Inverted number
+     */
+    public static int invert(int number) {
+        String stringNumber = String.valueOf(number);
+        stringNumber = new StringBuilder(stringNumber).reverse().toString();
+        return Integer.parseInt(stringNumber);
+    }
+
+    /**
+     * Method checks if number is palindrome
+     *
+     * @param number Number being checking
+     * @return True, if number is palindrome
+     */
+    public static boolean isPalindrome(int number) {
+        return number == invert(number);
+    }
+}
